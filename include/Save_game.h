92fc1b1735c90a88a06b93fe834475dc02043584
kevin <kevin.mouse@hotmail.com>
2024-06-19 19:55:58 -0600

    Add attributes to the save file for the collectables

--- include/Save_game.h
+++ include/Save_game.h
@@ -7,10 +7,16 @@
 class Save_game
 {
 private:
+    char _label[6];
     int _debt;
     int _money;
     char _highscore_names[10][12];
     int _highscore_values[10];
+    int _uniforms; // -> 32 bits, each bit represents a uniform (0 = locked, 1 = unlocked)
+    int _hats;
+    int _gloves;
+    int _themes;
+    int _minigames_collections[15]; // first bit represents if the collection is unlocked, the next bits represent the minigames unlocked in the collection
 public:
     Save_game();
     ~Save_game() = default;
@@ -26,6 +32,16 @@ public:
     void set_highscore_names(int pos, bn::string_view name);
     int highscore_values(int pos);
     void set_highscore_values(int pos, int value);
+    int uniforms();
+    void set_uniforms(int uniforms);
+    int hats();
+    void set_hats(int hats);
+    int gloves();
+    void set_gloves(int gloves);
+    int themes();
+    void set_themes(int themes);
+    int minigames_collections(int pos);
+    void set_minigames_collections(int pos, int value);
 };
 
 #endif
--- src/Save_game.cpp
+++ src/Save_game.cpp
@@ -1,10 +1,16 @@
 #include "Save_game.h"
 
 Save_game :: Save_game() :
+    _label(),
     _debt(),
     _money(),
     _highscore_names(),
-    _highscore_values()
+    _highscore_values(),
+    _uniforms(),
+    _hats(),
+    _gloves(),
+    _themes(),
+    _minigames_collections()
     {
 }
 
@@ -14,6 +20,36 @@ void Save_game::save() {
 
 void Save_game::load() {
     bn::sram::read(*this);
+
+    if(bn::string_view(_label) != bn::string_view("STEVE")) {
+        _label[0] = 'S';
+        _label[1] = 'T';
+        _label[2] = 'E';
+        _label[3] = 'V';
+        _label[4] = 'E';
+        _label[5] = '\0';
+
+        _debt = 0;
+        _money = 0;
+
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 12; j++) {
+                _highscore_names[i][j] = '\0';
+            }
+            _highscore_values[i] = 0;
+        }
+
+        _uniforms = 0;
+        _hats = 0;
+        _gloves = 0;
+        _themes = 0;
+
+        for(int i = 0; i < 15; i++) {
+            _minigames_collections[i] = 0;
+        }
+
+        save();
+    }
 }
 
 int Save_game::debt() {
@@ -53,4 +89,44 @@ int Save_game::highscore_values(int pos) {
 
 void Save_game::set_highscore_values(int pos, int value) {
     _highscore_values[pos] = value;
+}
+
+int Save_game::uniforms() {
+    return _uniforms;
+}
+
+void Save_game::set_uniforms(int uniforms) {
+    _uniforms = uniforms;
+}
+
+int Save_game::hats() {
+    return _hats;
+}
+
+void Save_game::set_hats(int hats) {
+    _hats = hats;
+}
+
+int Save_game::gloves() {
+    return _gloves;
+}
+
+void Save_game::set_gloves(int gloves) {
+    _gloves = gloves;
+}
+
+int Save_game::themes() {
+    return _themes;
+}
+
+void Save_game::set_themes(int themes) {
+    _themes = themes;
+}
+
+int Save_game::minigames_collections(int pos) {
+    return _minigames_collections[pos];
+}
+
+void Save_game::set_minigames_collections(int pos, int value) {
+    _minigames_collections[pos] = value;
 }
